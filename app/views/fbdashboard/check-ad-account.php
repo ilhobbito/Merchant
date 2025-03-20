dc052f54fa50fd04c8efe0b3ea5506abcd62edcc
Tim Tallberg <tim.tallis@hotmail.com>
2025-03-20 14:00:26 +0100

    Added similar routing for getPixel, check ad account and api-test

---
+++ app/views/fbdashboard/check-ad-account.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Check Ad Account</title>
+</head>
+<body>
+    <a href='/Merchant/public/fbdashboard'>Return</a>
+    <h4><?php echo $message ?></h4>
+</body>
+</html>
