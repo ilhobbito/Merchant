c905b1b91a8611e2393e23bc6b8c7799e6b7b8f5
Robin Jonsson <jonsson_robin@icloud.com>
2025-03-21 09:06:02 +0100

    Some changes to routing in Google ads

--- app/views/googleads/index.php
+++ app/views/googleads/index.php
@@ -12,7 +12,7 @@ if (!isset($_SESSION['access_token'])) {
 </head>
 <body>
     <h2>Welcome to Google Ads</h2>
-    <a href="googleads/createTestClient">Create Test Client</a><br>
+    <a href="createTestClient">Create Test Client</a><br>
     <a href="googleads/listCampaign">List campaigns</a><br>
     <a href="googleads/setTestBudget">Set Test Budget</a><br>
     <a href="googleads/listAccountsWithLibrary">List accounts</a>
